4eeeeca9ce8e3613fa5f2a4c14c197c402870f9e
rgoudinho <43451982+rgoudinho@users.noreply.github.com>
2023-10-29 14:29:05 -0300

    Revert "Feature/atividade 08"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
